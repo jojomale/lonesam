a068d5e201b055de0345d80db062bc73c5fa985e
Johanna L <johanna.lehr@bgr.de>
2023-01-30 20:05:07 +0100

    fix notebook execution in readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -82,4 +82,4 @@ napoleon_include_private_with_doc = True
 #                  'https://cdn.plot.ly/plotly-latest.min.js']
 
 nbsphinx_requirejs_path = 'https://cdn.plot.ly/plotly-latest.min.js'
-nbsphinx_execute = 'never'
+# nbsphinx_execute = 'never'
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -13,9 +13,7 @@ Welcome to dataqc's documentation!
    modules
    cmdline
    usage_demo
-   missing_file_handling
-   data_gap_handling
-   read_winddata
+   
 
 
 
